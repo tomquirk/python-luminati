c79737ddbf5a49bbcbc946979827a1c301e4658d
Tom Quirk <tomquirkacc@gmail.com>
2019-06-14 08:22:34 +1000

    fix str formatting

--- luminati.py
+++ luminati.py
@@ -12,8 +12,8 @@ class Luminati:
     def refresh_url(self):
         session_id = random()
         super_proxy_url = (
-            "http://%s-country-au-session-%s:%s@zproxy.lum-superproxy.io:%d"
-            % (self._username, f'country-{self._country}' if self._country else '', session_id, self._password, self._port)
+            "http://%s%s-session-%s:%s@zproxy.lum-superproxy.io:%d"
+            % (self._username, f'-country-{self._country}' if self._country else '', session_id, self._password, self._port)
         )
 
         self._current_proxy_url = super_proxy_url
